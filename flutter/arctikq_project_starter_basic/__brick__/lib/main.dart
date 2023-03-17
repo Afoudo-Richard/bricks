1ccbe9ebb2647737889c265768be8bfe05e1ae8f
Richard Afoudo <richardafoudo07@gmail.com>
2023-03-17 23:25:16 +0100

    updated brick

--- flutter/arctikq_project_starter_basic/__brick__/lib/main.dart
+++ flutter/arctikq_project_starter_basic/__brick__/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/widgets.dart';
 import 'package:{{project_name.snakeCase()}}/app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(App());
 }
