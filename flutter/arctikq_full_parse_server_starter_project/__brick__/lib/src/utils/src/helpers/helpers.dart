13a1f5d719d730bc96d5261344aee86bf3a66a43
Richard Afoudo <richardafoudo07@gmail.com>
2023-04-10 04:48:35 +0100

    updated arctikq parse

--- flutter/arctikq_full_parse_server_starter_project/__brick__/lib/src/utils/src/helpers/helpers.dart
+++ flutter/arctikq_full_parse_server_starter_project/__brick__/lib/src/utils/src/helpers/helpers.dart
@@ -4,3 +4,4 @@ export './src/responsive_helper.dart';
 export './src/capitalize_first.dart';
 export './src/debouncer.dart';
 export './src/translate_helper.dart';
+export './src/device_helper.dart';
